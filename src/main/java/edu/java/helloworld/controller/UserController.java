a24d709535be83437b8cd57b7d6704218c0ba944
trinhquangtoan2112 <trinhtoanquang@gmail.com>
2025-03-27 16:00:32 +0700

    add .

---
+++ src/main/java/edu/java/helloworld/controller/UserController.java
@@ -0,0 +1,25 @@
+package edu.java.helloworld.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.java.helloworld.dto.request.UserRequestDTO;
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @PostMapping("/")
+    public String addUser( @Valid @RequestBody UserRequestDTO userRequestDTO) {
+        
+        UserRequestDTO userDTO = new UserRequestDTO(userRequestDTO.getFirstName(), userRequestDTO.getLastName(), userRequestDTO.getEmail(),userRequestDTO.getPhone());
+        System.out.println(userDTO.getFirstName());
+        return "da nhan";
+    }
+    
+}
---
+++ src/main/java/edu/java/helloworld/dto/request/UserRequestDTO.java
@@ -0,0 +1,55 @@
+package edu.java.helloworld.dto.request;
+
+import java.io.Serializable;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public class UserRequestDTO implements Serializable {
+    @NotBlank(message = "khong duoc de trong first name")
+    private String firstName;
+    @NotNull(message = "khong duoc bo trong")
+    private String lastName;
+    @Email(message = "phai la email nhat dinh")
+    private String email;
+    @Pattern(regexp = "^\\d{10}$",message = "phone invalied format")
+    private String phone;
+
+
+    public UserRequestDTO(String firstName,String lastName, String email, String phone){
+      this.email=email;
+      this.firstName=firstName;
+      this.lastName=lastName;
+      this.phone=phone;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
